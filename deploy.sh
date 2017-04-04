1320bcb8a25fcb211b0dd99d8b9385337732f1a3
iambryancs <yanser25@gmail.com>
2017-04-04 15:14:20 +0800

    Update name

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-mkdir /var/jenkins_home/.ssh
+mkdir /var/jenkins_home/.ssh || true
 
 ssh-keyscan 172.17.0.1 >> /var/jenkins_home/.ssh/known_hosts
 cp ./keys/deploy_key /var/jenkins_home/.ssh/id_rsa
 chmod 600 /var/jenkins_home/.ssh/id_rsa
   
 ssh vagrant@172.17.0.1 <<EOF
-  docker rmi -f bryancs/git-jenkins-ci-cd || true
-  docker rm -f git-jenkins-ci-cd-demo-app || true
-  rm -rf ~/git-jenkins-ci-cd
+  docker rmi -f bryancs/git-jenkins-ci || true
+  docker rm -f git-jenkins-ci-demo-app || true
+  rm -rf ~/git-jenkins-ci
   cd ~
-  git clone https://github.com/iambryancs/git-jenkins-ci-cd.git
-  cd git-jenkins-ci-cd/app
-  docker build -t bryancs/git-jenkins-ci-cd .
-  docker run -d -p 8000:8000 --name git-jenkins-ci-cd-demo-app bryancs/git-jenkins-ci-cd
+  git clone https://github.com/iambryancs/git-jenkins-ci.git
+  cd git-jenkins-ci/app
+  docker build -t bryancs/git-jenkins-ci .
+  docker run -d -p 8000:8000 --name git-jenkins-ci-demo-app bryancs/git-jenkins-ci
   exit
 EOF
